86318e2d524a5333c4c979ba837ab4b0457addf5
Jonathan <jalcantara1990@gmail.com>
2017-12-22 00:09:42 +0100

    first component with React

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hola mundo!!");
+import React from 'react'
+import { render } from 'react-dom';
+import Media from './src/playlist/components/media';
+
+const app = document.getElementById('app');
+
+render(<Media />, app); // recibe dos parámetros, que renderizar y donde va a hacerlo.
