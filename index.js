de3b94a0d09b7a7879bf6bfd691a81b7c8f02715
Jonathan <jalcantara1990@gmail.com>
2017-12-22 01:59:12 +0100

    add proptypes and state

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ import Media from './src/playlist/components/media';
 
 const app = document.getElementById('app');
 
-render(<Media />, app); // recibe dos parámetros, que renderizar y donde va a hacerlo.
+render(<Media type="video" title="¿Qué es responsive design?" author="Leonidas Esteban" image="./images/covers/responsive.jpg"/>, app); // recibe dos parámetros, que renderizar y donde va a hacerlo.
--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,24 +1,49 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './media.css';
 
 class Media extends Component {
+  state = {
+    author: this.props.author
+  }
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     author: props.author
+  //   }
+
+  // }
+
+  handleClick = event => {
+    console.log(this.props.title);
+    this.setState({
+      author: 'Jonathan Alcántara'
+    })
+  }
   render() {
     return(
-      <div className="Media">
+      <div className="Media" onClick={this.handleClick}>
         <div className="Media-cover">
           <img 
             className="Media-image"
-            src="./images/covers/bitcoin.jpg" 
+            src={this.props.image}
             alt=""
             width={260}
             height={160}
           />
-          <h3 className="Media-title">¿Por qué aprender React?</h3>
-          <p className="Media-author">LeonidasEsteban</p>
+          <h3 className="Media-title">{this.props.title}</h3>
+          <p className="Media-author">{this.state.author}</p>
         </div>
       </div>
     )
   }
 }
 
-export default Media
+Media.propTypes = {
+  image: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string,
+  type: PropTypes.oneOf(['video', 'audio'])
+}
+
+export default Media;
