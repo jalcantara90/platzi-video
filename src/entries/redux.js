48c77b5fe02c750bfd9fe7be4fb320a61d7ca873
Jonathan <jalcantara1990@gmail.com>
2018-02-15 01:45:34 +0100

    first configuration of redux, initial state created

---
+++ src/entries/redux.js
@@ -0,0 +1,29 @@
+import { createStore } from 'redux';
+
+const $form = document.getElementById('form');
+$form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const data = new FormData($form);
+  const title = data.get('title');
+  console.log(title);
+}
+
+const initialState = [
+  {
+    "title": "Despacito",
+  },
+  {
+    "title": "One more time",
+  },
+  {
+    "title": "Echame la culpa",
+  }
+];
+
+const store = createStore(
+  (state) => state,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+)
